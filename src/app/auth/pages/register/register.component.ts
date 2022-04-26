7608c8d371945c6f3944487d5cd3d90c2d97b16a
Antonio Andrade <antoniomaresmares001gmail.com>
2022-04-25 21:52:50 -0500

    creacion de formularios, diseño

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
 
-  constructor() { }
+  miFormulario:FormGroup = this._fb.group({
+    name: ['Test prueba',[ Validators.required ]],
+    email:['prueba@gmail.com',[ Validators.required, Validators.email ]],
+    password:['123456', [ Validators.required ]]
+  })
 
-  ngOnInit(): void {
-  }
+  constructor( private _fb:FormBuilder,
+               private _router:Router ) { }
+
+  registro(){
+    
+  }         
+
 
 }
