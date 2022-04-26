401cda6a5e0d1102b7b2cf81ca95cbb73915e8c5
Antonio Andrade <antoniomaresmares001gmail.com>
2022-04-25 21:12:34 -0500

    creacion del login

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,21 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
   miFormulario:FormGroup = this._fb.group({
-      email:['test3@gmail.com', [ Validators.required, Validators.email ]],
-      password: ['123456', [Validators.required, Validators.minLength(6) ]]
-  })
-  
-  constructor( private _fb:FormBuilder ) { }
+    email:[ '',[ Validators.required, Validators.email ]],
+    password:[ '',[Validators.required, Validators.minLength(6)] ]
+    
+  })  
 
-  ngOnInit(): void {
+  constructor( private _fb:FormBuilder,
+               private _router:Router,
+              ) { }
+
+
+  login(){
+    
   }
 
+  send(){
+    console.log( this.miFormulario.value )
+    console.log( this.miFormulario.valid )
+  }
 }
